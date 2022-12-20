d08e44bbecec6931bee867be98be6e7c279fff45
dldvlpr <dormoy.dev@gmail.com>
2022-12-20 23:06:55 +0100

    first commit

---
+++ server/graphql/resolvers/books.resolver.ts
@@ -0,0 +1,26 @@
+import { GraphQLObjectType } from "graphql";
+import { IAddBookInput, IBook } from "./book.resolver.spec";
+
+let books : Array<IBook> = [
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
+];
+
+export default {
+    Query: {
+        listBooks: () => books,
+    },
+    Mutation: {
+        addBook: (_: GraphQLObjectType, { addBookInput }: IAddBookInput) => {
+            let newBook = { ...addBookInput };
+            books = [...books, newBook];
+            return newBook;
+        },
+    },
+};
---
+++ server/graphql/resolvers/index.ts
@@ -0,0 +1,4 @@
+import bookResolver from "./books.resolver";
+import {mergeResolvers} from "@graphql-tools/merge";
+
+export default mergeResolvers([bookResolver]);
